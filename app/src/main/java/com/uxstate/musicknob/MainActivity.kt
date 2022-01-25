fc78c331e204927100df3dac9a93c52a8bb42970
Antony <vontonnie@gmail.com>
2022-01-25 17:54:42 +0300

    Add MusicKnob Composable

--- app/src/main/java/com/uxstate/musicknob/MainActivity.kt
+++ app/src/main/java/com/uxstate/musicknob/MainActivity.kt
@@ -3,18 +3,29 @@ package com.uxstate.musicknob
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
 import com.uxstate.musicknob.ui.theme.MusicKnobTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MusicKnobTheme() {
+
+
+            }
 
         }
     }
+}
+
+
+@Composable
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = .25f,
+    onValueChange: (Float) -> Unit
+) {
+
 }
